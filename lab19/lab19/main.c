9f8b45ebbe1bd2b7b3b8515e9ed1871013634d22
Kaehvaman <v1r4k@mail.ru>
2024-11-01 21:07:49 +0400

    Create load() function

--- lab19/lab19/main.c
+++ lab19/lab19/main.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void load(int arr[]) {
-	FILE* file = fopen("\\data.txt", "r");
+void printarr(int arr[], int len) {
+	printf("[");
+	for (int i = 0; i < len; i++) {
+		printf("%d, ", arr[i]);
+	}
+	printf("\b\b]\n");
+}
+
+int load(int** parr) {
+	FILE* file = fopen("data.txt", "r");
 	if (file == NULL) {
 		puts("Файл не найден");
-		return;
+		exit(EXIT_FAILURE);
 	}
 
-	int len;
+	int len = 1;
 	fscanf_s(file, "%d", &len);
-	for (int i = 0; i < len; i++) {
-		fscanf_s(file, "%d", &arr[i]);
+
+	*parr = (int*)malloc(sizeof(int) * len);
+	if (*parr == NULL) {
+		printf_s("Not enough memory to load data\n");
+		exit(EXIT_FAILURE);
 	}
+
+	for (int i = 0; i < len; i++) {
+		fscanf_s(file, "%d", (*parr + i));
+	}
+
+	return len;
 }
 
 int main() {
-
+	int* parr;
+	int len;
+	len = load(&parr);
+	printarr(parr, len);
 	return 0;
 }

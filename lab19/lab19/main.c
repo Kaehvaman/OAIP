627d531df2a3dee5f51272b8fffa9b869f8403ae
Kaehvaman <v1r4k@mail.ru>
2024-11-01 21:29:18 +0400

    add saveRandomArray

--- lab19/lab19/main.c
+++ lab19/lab19/main.c
@@ -9,8 +9,8 @@ void printarr(int arr[], int len) {
 	printf("\b\b]\n");
 }
 
-int load(int** parr) {
-	FILE* file = fopen("data.txt", "r");
+int load(int** parr, char filename[]) {
+	FILE* file = fopen(filename, "r");
 	if (file == NULL) {
 		puts("Файл не найден");
 		exit(EXIT_FAILURE);
@@ -29,13 +29,48 @@ int load(int** parr) {
 		fscanf_s(file, "%d", (*parr + i));
 	}
 
+	fclose(file);
+
 	return len;
+}
+
+void save(int arr[], int len, char filename[]) {
+	FILE* file = fopen(filename, "w");
+	if (file == NULL) {
+		puts("Не удалось создать файл");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf_s(file, "%d\n", len);
+
+	for (int i = 0; i < len; i++) {
+		fprintf_s(file, "%d ", arr[i]);
+	}
+
+	fclose(file);
+}
+
+void saveRandomArray(int len, char filename[]) {
+	int* parr;
+	parr = (int*)malloc(sizeof(int) * len);
+	if (parr == NULL) {
+		printf_s("Not enough memory to load data\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < len; i++) {
+		parr[i] = rand() % 6;
+	}
+
+	save(parr, len, filename);
 }
 
 int main() {
 	int* parr;
 	int len;
-	len = load(&parr);
+	//saveRandomArray(30, "data.txt");
+	len = load(&parr, "data.txt");
+
 	printarr(parr, len);
 	return 0;
 }

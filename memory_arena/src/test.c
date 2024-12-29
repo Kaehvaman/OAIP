c6c5d3cf467dd4f2293b6fe87c242fcb65f4e810
Kaehvaman <v1r4k@mail.ru>
2024-12-29 21:17:00 +0400

    edit memory_arena test.c

--- memory_arena/src/test.c
+++ memory_arena/src/test.c
@@ -8,12 +8,11 @@
 
 int main() {
 
-	Arena arena = ArenaInit(1024 * 1024);
+	Arena arena = ArenaInit(1024 * 1024 * 1024);
 	
 	char* str1 = ArenaAllocAligned(&arena, 5, sizeof(char));
 	strcpy(str1, "Hell");
 	printf("%s\n", str1);
-
 
 	int* int2 = ArenaAllocAligned(&arena, 2 * sizeof(int), sizeof(int));
 	int2[0] = 123;
@@ -21,15 +20,14 @@ int main() {
 	printf("%d %d\n\n", int2[0], int2[1]);
 
 	for (int i = 0; i < 16; i++) {
-		printf("%d) %d\n", i, *((char*)(arena.buffer) + i));
+		printf("%2d) %d\n", i, *((char*)(arena.buffer) + i));
+	}
+
+	if (scanf("%s", str1)) {
+		ArenaDestroy(&arena);
 	}
 
 	scanf("%s", str1);
-
-	/*char* str3 = ArenaAllocAligned(&arena, 8, sizeof(char));
-	strcpy(str3, "ByeBye!!");
-	printf("%s", str3);*/
-	
 
 	return 0;
 }
